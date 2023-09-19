dee9319203676ca5e2d28a782b7ff4e79b02a35c
Naim <naimssj4@gmail.com>
2023-09-19 22:20:24 +0200

    cleanup, start ex_4 tp3

--- tp2/ex_2/Main.java
+++ tp2/ex_2/Main.java
@@ -13,6 +13,7 @@ public class Main {
     Scanner entree = new Scanner(System.in);
     System.out.println("Saisir un entier: ");
     int x = entree.nextInt();
+    entree.close();
 
     try{
       System.out.println("tab["+ x + "] = " + tab[x]);
@@ -22,22 +23,7 @@ public class Main {
   }
 
   public static void main(String[] args) {
-    try {
-      Date d1 = new Date(33, 10, 1885);
-      Date d2 = new Date(9, 15, 2005);
-      Date d3 = new Date(66, 30, 1966);
-      Date d4 = new Date(31, 9, 2000); //septembre n'a que 30 jours.
-      Date d5 = new Date(29, 2, 1000); //Fevrier n'a que 28 jours.
-    } catch(JourException e){
-      System.err.println("Erreur:");
-      System.err.println(e.getMessage());
-    } catch(MoisException e){
-      System.err.println("Erreur:");
-      System.err.println(e.getMessage());
-    }
-    catch (DateException e) {
-      System.err.println("Erreur:");
-      System.err.println(e.getMessage());
-    }
+    Message m = new Message();
+    m.contient("toto");
   }
 }
---
+++ tp2/ex_4/LigneInterditeException.java
@@ -0,0 +1,4 @@
+public class LigneInterditeException {
+
+  
+}
